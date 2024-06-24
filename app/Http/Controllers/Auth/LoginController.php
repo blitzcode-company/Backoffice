2f53ab9a220ce20fd722cf3fdb1a3ab217037f19
diegovega223 <diegovegaganachipi@gmail.com>
2024-06-24 20:52:38 -0300

    update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -39,7 +39,7 @@ class LoginController extends Controller
         RateLimiter::clear($this->throttleKey($request));
         $request->session()->regenerate();
 
-        return redirect()->intended('inicio');
+        return redirect()->intended('/');
     }
 
     public function logout()
--- routes/web.php
+++ routes/web.php
@@ -7,52 +7,49 @@ use Illuminate\Support\Facades\Route;
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
 
-
-Route::get('/', function () {
-    return view('inicio');
-})->name('inicio');
-
-Route::get('/usuarios', function () {
-    return view('usuarios');
-})->name('usuarios');
-
-Route::get('/canales', function () {
-    return view('canales');
-})->name('canales');
-
-Route::get('/videos', function () {
-    return view('videos');
-})->name('videos');
-
-Route::get('/publicidades', function () {
-    return view('publicidades');
-})->name('publicidades');
-
-Route::get('/estadisticas', function () {
-    return view('estadisticas');
-})->name('estadisticas');
-
-Route::get('/anuncios', function () {
-    return view('anuncios');
-})->name('anuncios');
-
-Route::get('/perfil', function () {
-    return view('perfil');
-})->name('perfil');
-
-
-Route::get('/ajustes', function () {
-    return view('ajustes');
-})->name('ajustes');
-
 Route::get('logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::middleware(['auth'])->group(function () {
- 
- 
     Route::prefix('blitzvideo')->group(function () {
         Route::get('/users', [UserController::class, 'listarTodosLosUsuarios']);
         Route::get('/users/{id}', [UserController::class, 'listarUsuarioPorId']);
         Route::get('/users/nombre/{nombre}', [UserController::class, 'listarUsuariosPorNombre']);
     });
+
+    Route::get('/', function () {
+        return view('inicio');
+    })->name('inicio');
+    
+    Route::get('/usuarios', function () {
+        return view('usuarios');
+    })->name('usuarios');
+    
+    Route::get('/canales', function () {
+        return view('canales');
+    })->name('canales');
+    
+    Route::get('/videos', function () {
+        return view('videos');
+    })->name('videos');
+    
+    Route::get('/publicidades', function () {
+        return view('publicidades');
+    })->name('publicidades');
+    
+    Route::get('/estadisticas', function () {
+        return view('estadisticas');
+    })->name('estadisticas');
+    
+    Route::get('/anuncios', function () {
+        return view('anuncios');
+    })->name('anuncios');
+    
+    Route::get('/perfil', function () {
+        return view('perfil');
+    })->name('perfil');
+    
+    
+    Route::get('/ajustes', function () {
+        return view('ajustes');
+    })->name('ajustes');
 });
