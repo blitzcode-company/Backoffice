fd6dcc64bd2fb12f0738e2b774b616469fca86b2
diegovega223 <diegovegaganachipi@gmail.com>
2024-06-24 17:52:45 -0300

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,11 +24,8 @@ class User extends Authenticatable implements LdapAuthenticatable
         'remember_token',
     ];
 
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
 }
